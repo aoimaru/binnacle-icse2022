94d478374741af68fe8cdcaddfcbb955cfecbe19
aoimaru <51444995+aoimaru@users.noreply.github.com>
2022-10-16 00:44:12 +0900

    [sucess] 4-phased-4-dockerfile-astsを汎用的にリファクタリング

--- datasets/4-abstracted-asts/generate.sh
+++ datasets/4-abstracted-asts/generate.sh
@@ -2,9 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# 第一引数の指定
+TARGET=$1
+
 # Build
 docker build -t binnacle/artifact:generate-phase-4 \
-  -f "${DIR}/generate/Dockerfile" "${DIR}/generate"
+  -f "${DIR}/generate/Dockerfile" "${DIR}/generate" --build-arg TARGET=$TARGET
 
 # Run with volume mounts
 time docker run \
